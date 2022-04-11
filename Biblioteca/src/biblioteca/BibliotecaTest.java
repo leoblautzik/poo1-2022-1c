7b4a5bdcfe290513ba5bb41651dde1baf1da31d4
leoblautzik <leoblautzik@gmail.com>
2022-04-11 09:59:27 -0300

    Clase 7: 11/04/22 Resolución Biblioteca

--- Biblioteca/src/biblioteca/BibliotecaTest.java
+++ Biblioteca/src/biblioteca/BibliotecaTest.java
@@ -10,18 +10,18 @@ public class BibliotecaTest {
 	public void SinLibrosTest() {
 		Biblioteca bibi = new Biblioteca(100);
 		assertEquals(0, bibi.cuantosLibros());
-		
+
 	}
-	
+
 	@Test
 	public void agregarLibroTest() {
 		Biblioteca bibi = new Biblioteca(100);
 		assertEquals(0, bibi.cuantosLibros());
 		bibi.agregar("Soy Java", "Pepe Java", 1200);
 		assertEquals(1, bibi.cuantosLibros());
-		
+
 	}
-	
+
 	@Test
 	public void consultarLibroEnLaPrimeraPosicionTest() {
 		Biblioteca bibi = new Biblioteca(100);
@@ -29,7 +29,7 @@ public class BibliotecaTest {
 		bibi.agregar("Soy Java", "Pepe Java", 1200);
 		assertEquals("Soy Java", bibi.libroEnLaPosicion(1));
 	}
-	
+
 	@Test(expected = Error.class)
 	public void consultarLibroEnPosicionCeroTest() {
 		Biblioteca bibi = new Biblioteca(100);
@@ -37,7 +37,7 @@ public class BibliotecaTest {
 		bibi.agregar("Soy Java", "Pepe Java", 1200);
 		assertEquals("Soy Java", bibi.libroEnLaPosicion(0));
 	}
-	
+
 	@Test
 	public void consultarLibroEnUltimaPosicionTest() {
 		Biblioteca bibi = new Biblioteca(2);
@@ -46,7 +46,7 @@ public class BibliotecaTest {
 		bibi.agregar("El Diego", "Maradona", 500);
 		assertEquals("El Diego", bibi.libroEnLaPosicion(2));
 	}
-	
+
 	@Test(expected = Error.class)
 	public void consultarLibroEnPosicionVaciaTest() {
 		Biblioteca bibi = new Biblioteca(4);
@@ -55,6 +55,7 @@ public class BibliotecaTest {
 		bibi.agregar("El Diego", "Maradona", 500);
 		assertNull(bibi.libroEnLaPosicion(3));
 	}
+
 	@Test
 	public void consultarLibroConMasPaginasTest() {
 		Biblioteca bibi = new Biblioteca(4);
@@ -66,7 +67,7 @@ public class BibliotecaTest {
 		Libro esperado = new Libro("Moby Dick", "Melville", 2500);
 		assertEquals(esperado, bibi.libroConMasPaginas());
 	}
-	
+
 	@Test
 	public void consultarLibrosDelAutorTest() {
 		Biblioteca bibi = new Biblioteca(100);
@@ -76,10 +77,9 @@ public class BibliotecaTest {
 		bibi.agregar("Moby Dick", "Melville", 2500);
 		bibi.agregar("Otro Libro", "Autor", 2400);
 		bibi.agregar("Soy C#", "Pepe Java", 1200);
-		assertEquals(2 , bibi.cuantosLibrosDelAutor("Pepe Java"));
+		assertEquals(2, bibi.cuantosLibrosDelAutor("Pepe Java"));
 	}
-	
-	
+
 	@Test
 	public void LibrosDelAutorTest() {
 		Biblioteca bibi = new Biblioteca(100);
@@ -89,13 +89,11 @@ public class BibliotecaTest {
 		bibi.agregar("Moby Dick", "Melville", 2500);
 		bibi.agregar("Otro Libro", "Autor", 2400);
 		bibi.agregar("Soy C#", "Pepe Java", 1200);
-		Libro [] esperado = {new Libro("Soy Java", "Pepe Java", 1200), 
-				new Libro("Soy C#", "Pepe Java", 1200)};
-							
-	
-		assertArrayEquals(esperado , bibi.librosDelAutor("Pepe Java"));
+		Libro[] esperado = { new Libro("Soy Java", "Pepe Java", 1200), new Libro("Soy C#", "Pepe Java", 1200) };
+
+		assertArrayEquals(esperado, bibi.librosDelAutor("Pepe Java"));
 	}
-	
+
 	@Test
 	public void tiempoEnLeerTodoTest() {
 		Biblioteca bibi = new Biblioteca(100);
@@ -105,7 +103,7 @@ public class BibliotecaTest {
 		bibi.agregar("Moby Dick", "Melville", 2500);
 		bibi.agregar("Otro Libro", "Autor", 2400);
 		bibi.agregar("Soy C#", "Pepe Java", 1200);
-		assertEquals((1200+500+2500+2400+1200), bibi.tiempoEnLeerTodosLosLibros());
+		assertEquals((1200 + 500 + 2500 + 2400 + 1200), bibi.tiempoEnLeerTodosLosLibros());
 	}
 
 }
